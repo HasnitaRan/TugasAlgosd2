f73aca5cb44e03e7e9dd47fcd2052e8e7e38dc79
HasnitaRan <92636895+HasnitaRan@users.noreply.github.com>
2022-06-06 17:48:24 +0700

    Add files via upload

--- src/SortStringHero.java
+++ src/SortStringHero.java
@@ -14,7 +14,7 @@ public class SortStringHero {
 
         for (int i = 0;i<this.hero.length;i++){
             for (int j=i+1;j<this.hero.length;j++){
-                if(this.hero[j].compareToIgnoreCase(this.hero[i]) <0) {
+                if(this.hero[j].compareToIgnoreCase(this.hero[i]) > 0) {
                     String temp = this.hero[i];
                     this.hero[i] = this.hero[j];
                     this.hero[j] = temp;
@@ -29,7 +29,7 @@ public class SortStringHero {
     }
 
     public static void main(String[] args) {
-        String[] superhero = {"Gatotkaca", "Ultraman", "Spiderman", "Batman", "Powerrangers"};
+        String[] superhero = {"Spiderman","Superman","Batman", "Wonderwoman", "Gundala"};
         SortStringHero sort = new SortStringHero();
         sort.setHero(superhero);
         sort.sortData();
